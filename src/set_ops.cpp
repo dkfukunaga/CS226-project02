88c3227d0fe399cd39797beac41db92b85c88dbb
dkfukunaga <dkfukunaga@gmail.com>
2025-09-19 22:02:19 -0700

    created stubs for set operation functions

---
+++ src/set_ops.cpp
@@ -0,0 +1,29 @@
+#include "set_ops.h"
+
+namespace setops {
+
+ValueSet        set_union(const ValueSet& A, const ValueSet& B) {
+    return {};
+}
+
+ValueSet        set_intersection(const ValueSet& A, const ValueSet& B) {
+    return {};
+}
+
+ValueSet        set_difference(const ValueSet& A, const ValueSet& B) {
+    return {};
+}
+
+ValueSet        set_symmetric_difference(const ValueSet& A, const ValueSet& B) {
+    return {};
+}
+
+ValuePairVec    cartesian_product(const ValueSet& A, const ValueSet& B) {
+    return {};
+}
+
+ValueSetVec     power_set(const ValueSet& A) {
+    return {};
+}
+
+} // namespace setops
