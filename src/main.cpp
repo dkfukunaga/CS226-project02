1b74e9f5a9f78cee2a8df35f75973eff15783626
dkfukunaga <dkfukunaga@gmail.com>
2025-09-19 18:13:13 -0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+/*
+  Class: CS226
+  Project Name: Programming Project 02
+   
+  Author: Dani Fukunaga
+   
+  Description: Write a program that takes as input two sets, A and B, and
+    outputs the following: A ∪ B, A ∩ B, A × B, A ⊕ B, 𝒫(A).
+
+    I want the program to take file input and output to both the console and a
+    file.
+
+  (style guide based on https://google.github.io/styleguide/cppguide.html)
+*/
+
+/* INCLUDES and NAMESPACE */
+#include <iostream>
+using namespace std;
+
+/* FUNCTION PROTOTYPES */
+void exampleFunction(int var1, string var2);
+
+int main() {
+	//put your main program here!
+
+	return 0;
+}
+
+/* FUNCTION DEFINITIONS */
+/* Description: 
+   Preconditions:
+   Postonditions:
+ */
+void exampleFunction(int var1, string var2) {
+	//function body here
+}
