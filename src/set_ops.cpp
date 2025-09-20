3d8dfa8535ef568d1025aa50dbea87f84da5610d
dkfukunaga <dkfukunaga@gmail.com>
2025-09-19 22:37:24 -0700

    implemented union and intersection

--- src/set_ops.cpp
+++ src/set_ops.cpp
@@ -3,11 +3,27 @@
 namespace setops {
 
 ValueSet        set_union(const ValueSet& A, const ValueSet& B) {
-    return {};
+    ValueSet result;
+    for (auto elem : A) {
+        result.insert(elem);
+    }
+    for (auto elem : B) {
+        result.insert(elem);
+    }
+    return result;
 }
 
 ValueSet        set_intersection(const ValueSet& A, const ValueSet& B) {
-    return {};
+    ValueSet result;
+    for (auto elem_a : A) {
+        for (auto elem_b : B) {
+            if (elem_a == elem_b) {
+                result.insert(elem_a);
+                break;
+            }
+        }
+    }
+    return result;
 }
 
 ValueSet        set_difference(const ValueSet& A, const ValueSet& B) {
